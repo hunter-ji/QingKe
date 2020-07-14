de9def8b3befd5cf60d21342e35a2ffb68471ded
Kuari <kuari@justmylife.cc>
2020-07-14 19:18:12 +0800

    feat:新增Home，修复process报错 替换原有首页视图 修改process报错 否

--- qingke/src/renderer/App.vue
+++ qingke/src/renderer/App.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-  export default {
-    name: 'qingke'
-  }
+export default {
+  name: "qingke"
+};
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
 </style>
---
+++ qingke/src/renderer/components/Home/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    Hello, World !
+  </div>
+</template>
